1205c2c24c80e6654aee8c8f0dc43763655bc339
peter <peter@kinamo.be>
2020-11-05 10:04:44 +0100

    Bugfix Remove output from view #1

--- src/RauweBieten/TwigStringyExtension.php
+++ src/RauweBieten/TwigStringyExtension.php
@@ -95,7 +95,6 @@ class TwigStringyExtension extends \Twig_Extension
     {
         return array_map(function ($method) {
             $filtername = StaticStringy::underscored(self::$prefix . $method);
-            print '- ' . $filtername . PHP_EOL;
             return new \Twig_Filter($filtername, [StaticStringy::class, $method]);
         }, self::$methods);
     }
